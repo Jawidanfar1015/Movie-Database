005b17d3179cf444ec57b595a955c3403e05eaa7
Jawid Noori <Hajikaka4030@gmail.com>
2022-06-21 21:05:38 -0700

    api route for updating the review added

--- server.js
+++ server.js
@@ -91,3 +91,25 @@ app.get('/api/movie-reviews', (req, res) => {
       });
     });
   });
+
+//  Update review name
+app.put('/api/review/:id', (req, res) => {
+    const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+    const params = [req.body.review, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
