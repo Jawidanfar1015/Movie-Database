df270bbc5936f79031388359869c3461dfb3d1d1
Jawid Noori <Hajikaka4030@gmail.com>
2022-06-21 21:00:34 -0700

    api route for creating a movie added

--- server.js
+++ server.js
@@ -20,3 +20,21 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the movies_db database.`)
   );
+
+  // Create a movie
+app.post('/api/new-movie', ({ body }, res) => {
+    const sql = `INSERT INTO movies (movie_name)
+      VALUES (?)`;
+    const params = [body.movie_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
