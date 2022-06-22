44adcdcb84e0b8eb4adf81330e9e8a03196eef10
Jawid Noori <Hajikaka4030@gmail.com>
2022-06-21 20:59:16 -0700

    database connected with the server

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
